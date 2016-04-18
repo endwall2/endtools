3142033b052c31386cc53bc411ed55caf99e2b3e
Endwall <endwall@noemail.org>
2016-04-17 22:03:45 -0600

    c03 17/04/2016

--- iplookup.py
+++ iplookup.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-######################################################################
+##########################################################################################################################################################################
 # Program: iplookup.py 
 # Type: python script
 # Version: 1.08
@@ -16,32 +16,29 @@
 # Required files: GeoIP.dat,GeoLiteCity.dat,GeoIPOrg.dat       
 # Downloaded from here: http://dev.maxmind.com/geoip/legacy/geolite/
 #
-######################################################################
-#                   INSTRUCTIONS
-######################################################################
-#    DEBIAN/UBUNTU/Trisquel/GnuSense
-#  $ apt-get install python python-pygeoip python2-pygeoip 
-#    ARCH-LINUX/PARABOLA/ANTERGOS/MANJARO/ARCHBANG
-#  $ pacman -S python python-pygeoip python2-pygeoip
-#    FEDORA/CENTOS/RHEL/SCIENTIFIC LINUX
-#  $ yum install python python-pygeoip
+####################################################################################################################################################################
+#                                          INSTRUCTIONS
+####################################################################################################################################################################
 #
 #  $ mkdir ~/geoip 
 #  $ cd ~/geoip
-#  $ wget http://b.1339.cf/gagzeqd.py
 #  $ wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
 #  $ wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
 #  $ wget http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz
 #  $ gunzip *.gz
-#  $ mv ~/geoip/gagzeqd.py ~/geoip/iplookup.py
-#  $ chmod u+wrx iplookup.py
+#  $ cp ~/Downloads/gagzeqd.py ~/geoip/iplookup.py
 #  $ ./iplookup.py 8.8.8.8
-#######################################################################
-#                 ACKNOWLEDGEMENTS
-#######################################################################
+#  
+#  edit/uncomment lines 141,142 and then perform 
+#  $ export PATH=$PATH:~/geoip
+#  $ iplookup.py 8.8.8.8
+#
+############################################################################################################################################################################
+#                                       ACKNOWLEDGEMENTS
+############################################################################################################################################################################
 #  The Endwall development team would like to acknowledge the work and efforts
 #  of Odilitime, who graciously hosted and promoted this firewall project.
-#  Without his efforts and his wonderful website www.endchan.xyz endwall.sh would not
+#  Without his efforts and his wonderful website www.endchan.xyz , endwall.sh would not
 #  exist in the public domain at all in any form. So thanks to Odilitime for inspiring this work
 #  and for hosting and promoting it. 
 #  
@@ -50,7 +47,7 @@
 #  Thank you also to early beta testers including a@a, and to other contributors 
 #  as well as to the detractors who helped to critique this work and to ultimately improve it.  
 #  
-#  We also acknowledge debian.paste.net and gitweb for their hosting services, 
+#  We also acknowledge paste.debian.net and gitweb for their hosting services, 
 #  without which distribution would be limited / impossible, so thank you.
 #
 #  https://www.endchan.xyz, http://paste.debian.net, http://gitweb2zl5eh7tp3.onion  
@@ -61,27 +58,86 @@
 #
 #  Endwall Development Team
 #
-#######################################################################
-#                LICENSE AGREEMENT
-#######################################################################
-#  1)  You have the freedom to study the code.
-#  2)  You have the freedom to distribute and share the code. 
-#  2 a) The License, Header and Instructions must be attached to the code when re-distributed.
-#  3)  You have the freedom to modify and improve the code.
-#  3 a) When modified or improved, during re-distribution you must attatch the LICENSE AGREEMENT in its entirety.   
-#  4)  You have the freedom to run the code on any computer of your choice.
-#  4 a) You are free to run as many simultaneous instances of this code on as many computers as you wish for as long as you wish with any degree of simultaneity. 
-#  6)  This program may be used for any purpose and in any context and any setting including for personal use, academic use and business or commercial use.
-#  7)  This software is distributed without any warranty and without any guaranty and the creators do not imply anything about its usefulness or efficacy.
-#  8) If you sustain finanical, material or physical loss as a result of using, running, or modifying this script you agree to 
-#     hold the creators the "Endwall Development Team" or the programers involved in its creation free from prosecution, free from indemnity, and free from liability
-#  9) If you find a significant flaw or make a significant improvement feel free to notify the original developers so that we may also
-#     include your improvement in the next release; you are not obligated to do this but we would enjoy this courtesy tremendously.   
-##################################################################################################
+##############################################################################################################################################################################
+#                               LICENSE AGREEMENT  
+##############################################################################################################################################################################
+#  BEGINNING OF LICENSE AGREMENT
+#  TITLE:  THE ENDWALL END USER LICENSE AGREEMENT (EULA) 
+#  VERSION: 1.04 
+#  VERSION DATE: April 11, 2016
+#   
+#  WHAT CONSTITUES "USE"? WHAT IS A "USER"?
+#  0) a) Use of this program means the ability to study, posses, run, copy, modify, publish, distribute and sell the code as included in all lines of this file,
+#        in text format or as a binary file consituting this particular program or its compiled binary machine code form, as well as the the performance 
+#        of these aforementioned actions and activities. 
+#  0) b) A user of this program is any individual who has been granted use as defined in section 0) a) of the LICENSE AGREEMENT, and is granted to those individuals listed in section 1.
+#  WHO MAY USE THIS PROGRAM ?
+#  1) a) This program may be used by any living human being, any person, any corporation, any company, and by any sentient individual with the willingness and ability to do so.
+#  1) b) This program may be used by any citizen or resident of any country, and by any human being without citizenship or residency.
+#  1) c) This program may be used by any civilian, military officer, government agent, private citizen, public official, soveriegn, monarch, head of state,
+#        dignitary, ambassdor, noble, commoner, clergy, layity, and generally all classes and ranks of people, persons, or human beings mentioned and those not mentioned.
+#  1) d) This program may be used by any human being of any gender, including men, women, and any other gender not mentioned.       
+#  1) e) This program may be used by anyone of any afiliation, political viewpoint, political affiliation, religious belief, religious affiliation, and by those of non-belief or non affiliation.
+#  1) f) This program may be used by any person of any race, ethnicity, identity, origin, genetic makeup, physical apperance, mental ability, and by those of any other physical 
+#        or non physical characteristics of differentiation.
+#  1) g) This program may be used by any human being of any sexual orientation, including heterosexual, homosexual, bisexual, asexual, or any other sexual orientation not mentioned.
+#  1) h) This program may be used by anyone. 
+#  WHERE MAY A USER USE THIS PROGRAM ?
+#  2) a) This program may be used in any country, in any geographic location of the planet Earth, in any marine or maritime environment, at sea, subsea, in a submarine, underground,
+#        in the air, in an airplane, derigible, blimp, or balloon, and at any distance from the surface of the planet Earth, including in orbit about the Earth or the Moon,
+#        on a satellite orbiting about the Earth or about any planet, on any space transport vehicle, and anywhere in the solar system including the Moon, Mars, and all other solar system planets not listed.  
+#  2) b) This program may be used in any residential, commercial, business, and governmental property or location and in all public and private spaces. 
+#  2) c) This program may be used anywhere.
+#  IN WHAT CONTEXT OR CIRCUMSTANCES MAY A USER USE THIS PROGRAM?
+#  3)  This program may be used by any person, human being or sentient individual for any purpose and in any context and in any setting including for personal use, academic use,
+#      business use, commercial use, government use, non-governmental organization use, non-profit organization use, military use, civilian use, and generally any other use 
+#      not specifically mentioned.
+#  WHAT MAY A "USER" DO WITH THIS PROGRAM ?
+#  4) Any user of this program is granted the freedom to study the code.
+#  5) a) Any user of this program is granted the freedom to distribute, publish, share the code with any neighbor of their choice electronically or by any other method of transmission. 
+#  5) b) The LICENCSE AGREEMENT, ACKNOWLEDGEMENTS, Header and Instructions must remain attached to the code when re-distributed.
+#  5) c) Any user of this program is granted the freedom to sell this software as distributed or to bundle it with other software or salable goods.
+#  6) a) Any user of this program is granted the freedom to modify and improve the code.
+#  6) b) When modified or improved, any user of this program is granted the freedom of re-distribution of their modified code if and only if the user attatchs the LICENSE AGREEMENT
+#        in its entirety to their modified code before re-distribution.
+#  6) c) Any user of this software is granted the freedom to sell their modified copy of this software or to bundle their modified copy with other software or salable goods.
+#  7) a) Any user of this program is granted the freedom to run this code on any computer of their choice.
+#  7) b) Any user of this program is granted the freedom to run as many simultaneous instances of this code, on as many computers as they are able to and desire, and for as long as they desire and are
+#        able to do so with any degree of simultaneity in use. 
+#  WHAT MUST A "USER" NOT DO WITH THIS PROGRAM ?
+#  8) Any user of this program is not granted the freedom to procur a patent for the methods presented in this software, and agrees not to do so.
+#  9) Any user of this program is not granted the freedom to arbitrarily procur a copyright on this software as presented, and agrees not to do so.
+#  10) Any user of this program is not granted the freedom to obtain or retain intelectual property rights on this software as presented and agrees not to do so.
+#  11) a) Any user of this program may use this software as part of a patented process, as a substitutable input into the process; however the user agrees not to attempt to patent this software as part of their patented process. 
+#      b) This software is a tool, like a hammer, and may be used in a process which applies for and gains a patent, as a substitutable input into the process;
+#         however the software tool itself may not be included in the patent or covered in the patent as a novel invention, and the user agrees not to do this and not to attempt to do this.
+#  WHO GRANTS THESE FREEDOMS ?
+#  10) The creators of this software are the original developer, and anyone listed as being a member of the Endwall Development Team, as well as ancillary contributors, and user modifiers and developers of the software. 
+#  11) The aformentioned freedoms of use listed in sections 4),5),6),and 7) are granted by the creators of this software and the Endwall Development Team to any qualifying user listed in section 1) and 
+#      comporting with any restrictions or qualifications mentioned in sections 2), 3), 8), 9), 10) and 11) of this LICENSE AGREEMENT.
+#  WHAT RELATIONSHIP DO THE USERS HAVE WITH THE CREATORS OF THE SOFTWARE ?
+#  12)  This software is distributed without any warranty and without any guaranty and the creators do not imply anything about its usefulness or efficacy.
+#  13)  If the user suffers or sustains financial loss, informational loss, material loss, physical loss or data loss as a result of using, running, or modifying this software 
+#       the user agrees that they will hold the creators of this software, the "Endwall Development Team" and the programers involved in its creation, free from prosecution, 
+#       free from indemnity, and free from liability, and will not attempt to seek restitution or renumeration for any such loss real or imagined.
+#  END OF LICENSE AGREEMENT
+##################################################################################################################################################################################
+#  ADITIONAL NOTES:
+#  14)  If a user finds a significant flaw or makes a significant improvement to this software, please feel free to notify the original developers so that we may also
+#       include your user improvement in the next release; users are not obligated to do this, but we would enjoy this courtesy tremendously.
+#
+#  15)  Sections 0) a) 0) b) and 1) a) are sufficient for use; however sections 1) b) through 1) h) are presented to clarify 1 a) and to enforce non-discrimination and non-exlusion of use.  
+#       For example some people may choose to redefine the meaning of the words "person" "human being" or "sentient individual" to exclude certain types of people.
+#       This would be deemed unacceptable and is specifically rejected by the enumeration presented.  If the wording presented is problematic please contact us and suggest a change,
+#       and it will be taken into consideration.  
+#################################################################################################################################################################################
 
 import sys
 import os
 import pygeoip
+
+#path = "/home/user/geoip"
+#os.chdir( path ) # change to dir with dat files
 
 gcount=pygeoip.GeoIP('GeoIP.dat')
 gcit=pygeoip.GeoIP('GeoLiteCity.dat')
