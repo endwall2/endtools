d75f9ff1942d07f667ac26b0397a67a24b855014
Endwall <endwall@noemail.org>
2016-05-04 17:58:19 -0600

    c02 04/05/2016

--- spamlogz.sh
+++ spamlogz.sh
@@ -2,9 +2,10 @@
 ######################################################################################################################################################################
 # Program:  spamlogz.sh
 # Type: Bourne shell script
-# Version: 1.06
-# Revision Date: Apr 16 2016
 # Author: The Endwall Development Team
+# Creation Date: February 13, 2016
+# Version: 1.06
+# Revision Date: May 04, 2016
 # Copyright: The Endwall Development Team 2016
 #
 # Description: Print log file of flagged terms
@@ -20,39 +21,40 @@
 #               # ./spamlogz.sh
 #               # ./spamlogz.sh Jan 5
 #
-#####################################################################################################################################################################
-#####################################################################################################################################################################
-#                                   ACKNOWLEDGEMENTS
-######################################################################################################################################################################
-#  The Endwall development team would like to acknowledge the work and efforts
-#  of Odilitime, who graciously hosted and promoted this firewall project.
-#  Without his efforts and his wonderful website www.endchan.xyz , endwall.sh would not
-#  exist in the public domain at all in any form. So thanks to Odilitime for inspiring this work
+##############################################################################################################################################################################
+##############################################################################################################################################################################
+#                                         ACKNOWLEDGEMENTS
+##############################################################################################################################################################################
+#  The Endware Development Team would like to acknowledge the work and efforts
+#  of OdiliTime, who graciously hosted and promoted this firewall project.
+#  Without his efforts and his wonderful website www.endchan.xyz , the Endware Suite including Endwall would not
+#  exist in the public domain at all in any form. So thanks to OdiliTime for inspiring this work
 #  and for hosting and promoting it. 
 #  
-#  Endwall,Endsets,Endlists, and Endtools are named in honor of Endchan.
+#  The Endware Suite including Endwall,Endsets,Endlists,Endtools, Endloads and Endtube are named in honor of Endchan.
 #
 #  Thank you also to early beta testers including a@a, and to other contributors 
 #  as well as to the detractors who helped to critique this work and to ultimately improve it.  
 #  
-#  We also acknowledge paste.debian.net and gitweb for their hosting services, 
-#  without which distribution would be limited / impossible, so thank you.
+#  We also acknowledge paste.debian.net, ix.io and gitweb for their hosting services, 
+#  without which distribution would be limited, so thank you.
 #
-#  https://www.endchan.xyz, http://paste.debian.net, http://gitweb2zl5eh7tp3.onion  
+#  https://www.endchan.xyz, http://paste.debian.net, http://gitweb2zl5eh7tp3.onion , http://ix.io  
 #
 #  We salute you! 
 #  
 #  In the end, may it all end well.
 #
-#  Endwall Development Team
-#
+#  The Endware Development Team
+##############################################################################################################################################
 ##############################################################################################################################################################################
 #                               LICENSE AGREEMENT  
 ##############################################################################################################################################################################
 #  BEGINNING OF LICENSE AGREMENT
-#  TITLE:  THE ENDWALL END USER LICENSE AGREEMENT (EULA) 
-#  VERSION: 1.04 
-#  VERSION DATE: April 11, 2016
+#  TITLE:  THE ENDWARE END USER LICENSE AGREEMENT (EULA) 
+#  CREATION DATE: MARCH 19, 2016
+#  VERSION: 1.07 
+#  VERSION DATE: MAY 5, 2016
 #   
 #  WHAT CONSTITUES "USE"? WHAT IS A "USER"?
 #  0) a) Use of this program means the ability to study, posses, run, copy, modify, publish, distribute and sell the code as included in all lines of this file,
@@ -63,7 +65,7 @@
 #  1) a) This program may be used by any living human being, any person, any corporation, any company, and by any sentient individual with the willingness and ability to do so.
 #  1) b) This program may be used by any citizen or resident of any country, and by any human being without citizenship or residency.
 #  1) c) This program may be used by any civilian, military officer, government agent, private citizen, public official, soveriegn, monarch, head of state,
-#        dignitary, ambassdor, noble, commoner, clergy, layity, and generally all classes and ranks of people, persons, or human beings mentioned and those not mentioned.
+#        dignitary, ambassdor, noble, commoner, clergy, layity, and generally all classes and ranks of people, persons, and human beings mentioned and those not mentioned.
 #  1) d) This program may be used by any human being of any gender, including men, women, and any other gender not mentioned.       
 #  1) e) This program may be used by anyone of any afiliation, political viewpoint, political affiliation, religious belief, religious affiliation, and by those of non-belief or non affiliation.
 #  1) f) This program may be used by any person of any race, ethnicity, identity, origin, genetic makeup, physical apperance, mental ability, and by those of any other physical 
@@ -72,7 +74,7 @@
 #  1) h) This program may be used by anyone. 
 #  WHERE MAY A USER USE THIS PROGRAM ?
 #  2) a) This program may be used in any country, in any geographic location of the planet Earth, in any marine or maritime environment, at sea, subsea, in a submarine, underground,
-#        in the air, in an airplane, derigible, blimp, or balloon, and at any distance from the surface of the planet Earth, including in orbit about the Earth or the Moon,
+#        in the air, in an airplane, dirigible, blimp, or balloon, and at any distance from the surface of the planet Earth, including in orbit about the Earth or the Moon,
 #        on a satellite orbiting about the Earth or about any planet, on any space transport vehicle, and anywhere in the solar system including the Moon, Mars, and all other solar system planets not listed.  
 #  2) b) This program may be used in any residential, commercial, business, and governmental property or location and in all public and private spaces. 
 #  2) c) This program may be used anywhere.
@@ -82,13 +84,13 @@
 #      not specifically mentioned.
 #  WHAT MAY A "USER" DO WITH THIS PROGRAM ?
 #  4) Any user of this program is granted the freedom to study the code.
-#  5) a) Any user of this program is granted the freedom to distribute, publish, share the code with any neighbor of their choice electronically or by any other method of transmission. 
-#  5) b) The LICENCSE AGREEMENT, ACKNOWLEDGEMENTS, Header and Instructions must remain attached to the code when re-distributed.
-#  5) c) Any user of this program is granted the freedom to sell this software as distributed or to bundle it with other software or salable goods.
+#  5) a) Any user of this program is granted the freedom to distribute, publish, and share the code with any neighbor of their choice electronically or by any other method of transmission. 
+#  5) b) The LICENCSE AGREEMENT, ACKNOWLEDGEMENTS, Header and Instructions must remain attached to the code in their entirety when re-distributed.
+#  5) c) Any user of this program is granted the freedom to sell this software as distributed or to bundle it with other software or saleable goods.
 #  6) a) Any user of this program is granted the freedom to modify and improve the code.
 #  6) b) When modified or improved, any user of this program is granted the freedom of re-distribution of their modified code if and only if the user attatchs the LICENSE AGREEMENT
 #        in its entirety to their modified code before re-distribution.
-#  6) c) Any user of this software is granted the freedom to sell their modified copy of this software or to bundle their modified copy with other software or salable goods.
+#  6) c) Any user of this software is granted the freedom to sell their modified copy of this software or to bundle their modified copy with other software or saleable goods.
 #  7) a) Any user of this program is granted the freedom to run this code on any computer of their choice.
 #  7) b) Any user of this program is granted the freedom to run as many simultaneous instances of this code, on as many computers as they are able to and desire, and for as long as they desire and are
 #        able to do so with any degree of simultaneity in use. 
@@ -100,13 +102,13 @@
 #      b) This software is a tool, like a hammer, and may be used in a process which applies for and gains a patent, as a substitutable input into the process;
 #         however the software tool itself may not be included in the patent or covered in the patent as a novel invention, and the user agrees not to do this and not to attempt to do this.
 #  WHO GRANTS THESE FREEDOMS ?
-#  10) The creators of this software are the original developer, and anyone listed as being a member of the Endwall Development Team, as well as ancillary contributors, and user modifiers and developers of the software. 
+#  10) The creators of this software are the original developer,"Endwall", and anyone listed as being a member of "The Endware Development Team", as well as ancillary contributors, and user modifiers and developers of the software. 
 #  11) The aformentioned freedoms of use listed in sections 4),5),6),and 7) are granted by the creators of this software and the Endwall Development Team to any qualifying user listed in section 1) and 
-#      comporting with any restrictions or qualifications mentioned in sections 2), 3), 8), 9), 10) and 11) of this LICENSE AGREEMENT.
+#      comporting with any restrictions and qualifications mentioned in sections 2), 3), 8), 9), 10) and 11) of this LICENSE AGREEMENT.
 #  WHAT RELATIONSHIP DO THE USERS HAVE WITH THE CREATORS OF THE SOFTWARE ?
 #  12)  This software is distributed without any warranty and without any guaranty and the creators do not imply anything about its usefulness or efficacy.
 #  13)  If the user suffers or sustains financial loss, informational loss, material loss, physical loss or data loss as a result of using, running, or modifying this software 
-#       the user agrees that they will hold the creators of this software, the "Endwall Development Team" and the programers involved in its creation, free from prosecution, 
+#       the user agrees that they will hold the creators of this software, "The Endware Development Team", "Endwall", and the programers involved in its creation, free from prosecution, 
 #       free from indemnity, and free from liability, and will not attempt to seek restitution or renumeration for any such loss real or imagined.
 #  END OF LICENSE AGREEMENT
 ##################################################################################################################################################################################
