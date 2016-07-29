ffb8a08813378b7637851d24c497f62790991c97
Endwall <endwall@noemail.org>
2016-07-28 21:51:59 -0600

    c02 28/07/2016

--- endnode.sh
+++ endnode.sh
@@ -309,7 +309,7 @@ echo "$UA"
 # generate a random number time delay
 
 # check tor project ip
-torsocks curl -A "UA" https://check.torproject.org/ > $check_tor
+torsocks curl -A "$UA" https://check.torproject.org/ > $check_tor
 exit_address=$(grep -ah "Your IP" $check_tor | awk 'BEGIN {FS=">"} {print $3}' | awk 'BEGIN {FS="<"} {print $1}' )
 echo "TOR exit node is "$exit_address" "
 "$lookup_tool" "$exit_address" 
